1b88ac46f8f9b1112e5cd15bb609a9da1163f12d
TaksimIslam3847 <islan.taxim@gmail.com>
2023-04-20 13:55:49 -0400

    Initial commit

---
+++ TaksimIslA/src/main/java/taksim/islam/n01523847/ta/a/TakActivityA.java
@@ -0,0 +1,14 @@
+package taksim.islam.n01523847.ta.a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TakActivityA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
